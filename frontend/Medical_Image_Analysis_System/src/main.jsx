efa2093b3d0c247ca61bc2f93f15017373a362ca
AbhayTyagi1195 <abhay.22b1541195@abes.ac.in>
2026-02-20 01:16:25 +0530

    Second Model Added.

--- frontend/Medical_Image_Analysis_System/src/main.jsx
+++ frontend/Medical_Image_Analysis_System/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-// import 'bootstrap/dist/css/bootstrap.min.css'  // Add this line
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './components/Dashboard.css';
+import './components/DiseaseAnalysis.css';
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
